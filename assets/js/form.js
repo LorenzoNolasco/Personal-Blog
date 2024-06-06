0053ebd32220518a5c3496d5ea5e98e7d3a3fa2f
Lorenzo Nolasco <Lorenzonolasco23@gmail.com>
2024-06-06 15:03:40 -0600

    set up input and working on storing info

---
+++ assets/js/form.js
@@ -0,0 +1,40 @@
+let postCounter = 0;
+// console.log(postCounter);
+
+function recordUserInput(event) {
+  event.preventDefault();
+
+  //sets variables submitted by the user
+  const username = document.getElementById("userName").value;
+  const email = document.getElementById("userEmail").value;
+  const userPost = document.getElementById("userPost").value;
+  console.log(username);
+  console.log(email);
+  console.log(userPost);
+
+  const userPosted = {
+    userName: username,
+    email: email,
+    userPost: userPost,
+  };
+  console.log(userPosted);
+
+  usersPostStored(userPosted);
+}
+
+function usersPostStored(userPosted) {
+  const userPostArray = [];
+
+  userPostArray[postCounter].push(userPosted);
+  // console.log(`objects in userPostArray: ${userPostArray.length}`);
+  console.log(`objects in userPostArray: ${userPostArray}`);
+  // console.log(userPostArray[0]);
+  // console.log(userPostArray[1]);
+  // console.log(userPostArray.length)
+
+  const storeUserPostArray = JSON.stringify(userPostArray);
+  localStorage.setItem = ("UserPost", storeUserPostArray);
+
+  postCounter++;
+  console.log(postCounter);
+}
